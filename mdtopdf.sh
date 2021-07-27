73385762d00856fc208421a0f5fce8d9707d1683
antipro <antipro@163.com>
2021-07-27 16:31:42 +0800

    UPDATE

--- mdtopdf.sh
+++ mdtopdf.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-pandoc --defaults=/apps/pandoc-config.yaml -o out.pdf --title=OK $1
+pandoc --pdf-engine=xelatex \
+  --wrap=preserve \
+  -V CJKmainfont="WenQuanYi Micro Hei" \
+  -V geometry:"top=2cm, bottom=1.5cm, left=2cm, right=2cm" \
+  --highlight-style tango \
+  --title $2 --toc -N \
+  -o $2.pdf $1
